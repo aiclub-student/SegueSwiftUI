ec70551e2e60cc4d8e8d484c3475138d09fa34b3
Amit Gupta <amit@gprof.com>
2022-08-18 20:05:31 -0700

    Initial Commit

---
+++ SegueSwiftUI/SegueSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegueSwiftUIApp.swift
+//  SegueSwiftUI
+//
+//  Created by Amit Gupta on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SegueSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
